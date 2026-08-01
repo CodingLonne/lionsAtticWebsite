23bf3b8fe26ef6741797c9901f034bca36fb3fdb
BuildTools <leonne.snel@gmail.com>
2026-08-01 17:10:52 +0200

    api helper class and updated gitignore

---
+++ messyattic/api/api-helper.php
@@ -0,0 +1,14 @@
+<?php
+function get_method () {
+	return $_SERVER['REQUEST_METHOD'];
+}
+
+function get_request_data () {
+	return array_merge(empty($_POST) ? array() : $_POST, (array) json_decode(file_get_contents('php://input'), true), $_GET);
+}
+
+function send_response ($response, $code = 200) {
+	http_response_code($code);
+	die(json_encode($response));
+}
+?>
